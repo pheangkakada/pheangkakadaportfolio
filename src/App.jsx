9ac3f995c02230c5778a1f6243d9459419634974
pheangkakada <159972975+pheangkakada@users.noreply.github.com>
2026-06-18 17:00:21 +0700

    New Push

--- src/App.jsx
+++ src/App.jsx
@@ -26,7 +26,7 @@ function Home({ theme, toggleTheme, portfolioMode, setPortfolioMode }) {
       data-portfolio-mode={portfolioMode}
       className={`min-h-screen bg-slate-950 text-slate-200 font-sans ${selectionClass}`}
     >
-      {/* <Navbar theme={theme} onToggleTheme={toggleTheme} /> */}
+      <Navbar theme={theme} onToggleTheme={toggleTheme} />
 
       <main>
         <Hero activeMode={portfolioMode} onModeChange={setPortfolioMode} />
